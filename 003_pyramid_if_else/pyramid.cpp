73505ec0cbe1594b1d0e208d018811d81f2d7144
Nikhil Kumar <113007497+demonikkk@users.noreply.github.com>
2025-02-27 10:54:37 +0530

    Add files via upload

---
+++ 003_pyramid_if_else/pyramid.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i=0; i<3; i++){
+        for(int j=0; j<5; j++){
+            if(i==0&(j==0|j==1|j==3|j==4) | i==1&(j==0|j==4)){
+                cout<<" ";
+                continue;
+            }
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+return 0;
+}
